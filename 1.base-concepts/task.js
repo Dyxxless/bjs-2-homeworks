641a56a18c8738c7c61fee50df5d63b440064a02
Dyxxless <79612696784@yandex.ru>
2023-02-07 22:38:44 +0300

    Add Homework 1.Base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,15 @@
-"use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let d = b * b - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if(d > 0){
+    arr[0] = (-b + Math.sqrt(d))/(2 * a);
+    arr[1] = (-b - Math.sqrt(d))/(2 * a);
+  }
+
+  else if(d == 0){
+    arr[0] = (-b )/(2 * a);
+  }
+
+  return arr;
 }
