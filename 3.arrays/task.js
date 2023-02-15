4c8f55bba322354ded3dcccd504a7b87f086b1a7
dyxxless <79612696784@yandex.ru>
2023-02-15 21:38:12 +0300

    Add Homework 3.Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
+  let result;
+  if (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
+    result = true;
+  } else result = false;
   
+  return result;
 }
 
-function getUsersNamesInAgeRange(users, gender) {
+function advancedFilter(arr) {
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
   
+  return resultArr;
 }
